48adab2af7f77f516224317487c854805ff00e14
Hunain Siddiqui <musiddiqui@paypal.com>
2025-05-02 00:27:12 +0100

    changed jsx to js

--- client/profiledata.js
+++ client/profiledata.js
@@ -1,21 +1,21 @@
-// profileData.js
-let profiles = [];
-let currentId = 1;
+    // profileData.js
+    let profiles = [];
+    let currentId = 1;
 
-export function addProfile(profile) {
-  const newProfile = { id: currentId++, ...profile };
-  profiles.push(newProfile);
-  console.log("Profile added:", newProfile);
-}
+    export function addProfile(profile) {
+    const newProfile = { id: currentId++, ...profile };
+    profiles.push(newProfile);
+    console.log("Profile added:", newProfile);
+    }
 
-export function findUserById(id) {
-  return profiles.find(profile => profile.id === id);
-}
+    export function findUserById(id) {
+    return profiles.find(profile => profile.id === id);
+    }
 
-export function findUserByUsernameAndPassword(username, password) {
-  return profiles.find(p => p.username === username && p.password === password);
-}
+    export function findUserByUsernameAndPassword(username, password) {
+    return profiles.find(p => p.username === username && p.password === password);
+    }
 
-export function getProfiles() {
-  return profiles;
-}
+    export function getProfiles() {
+    return profiles;
+    }
