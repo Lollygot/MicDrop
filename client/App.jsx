a6b498f705ef286c658f4f55c4e89d1657a604b2
msevindik <msevindik@paypal.com>
2025-05-01 14:26:16 +0100

    testing api calls

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(JSON.stringify(jsonData, null, 2)); // Pretty-print JSON
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>This is some test text</Text>
+      <Text>{data || error}</Text>
+
     </View>
   );
 }
