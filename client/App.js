4b2029ad0874c497e2683205f3e3a99fe10c5076
msevindik <msevindik@paypal.com>
2025-05-01 17:31:06 +0100

    working apple maps

--- client/App.js
+++ client/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import WebView from 'react-native-webview';
 import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ html: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>' }}
-    />
-  )
-};
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   map: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
+
 
 
